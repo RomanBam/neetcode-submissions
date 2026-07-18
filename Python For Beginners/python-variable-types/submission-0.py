57ee31f95e70ce833f03f41a3c63f4d358bbdd21
Roman Bamrah <romanbamrah@gmail.com>
2026-07-18 18:53:03 -0400

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,14 @@
+integer_type = 5
+float_type = 2.3
+bool_type = True
+string_type = "Hi"
+list_type = [1,2,3]
+
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
